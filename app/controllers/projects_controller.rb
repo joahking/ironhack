3c769045723ff041b45873ab9b7fa1ef8cc4e679
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2016-11-09 11:09:28 +0100

    Projects new and create

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,4 +10,20 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find params[:id]
   end
+
+  # GET /projects/new
+  # Show a form to create a project
+  def new
+    @project = Project.new
+  end
+
+  # POST /projects
+  # Creates a project
+  def create
+    @project = Project.new(name: params[:project][:name],
+                           description: params[:project][:description])
+    @project.save
+
+    redirect_to project_path(@project.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'projects', to: 'projects#index'
+  get 'projects/new', to: 'projects#new', as: :new_project
   get 'projects/:id', to: 'projects#show', as: :project
+
+  post 'projects', to: 'projects#create'
 end
