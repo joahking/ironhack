43fad35ac9371d16d0ccc22078954fcef68b8b0e
Joaquin Rivera Padron <joaquin.rivera@xing.com>
2016-11-09 09:50:03 +0100

    Projects show route, controller action and an empty view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,13 @@
 class ProjectsController < ApplicationController
+  # GET /projects
+  # Lists all projects
   def index
     @projects = Project.order(created_at: :desc).limit(10)
+  end
+
+  # GET /projects/:id
+  # Show details of a project
+  def show
+    @project = Project.find params[:id]
   end
 end
